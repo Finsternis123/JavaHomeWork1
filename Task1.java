806f4c4d86e8157398e1390a7a67537a26b20b11
Vladislav <zomba4o4ek100@gmail.com>
2023-04-21 16:34:24 +0300

    First Task complete

---
+++ Task1.java
@@ -0,0 +1,12 @@
+package JavaWork.JavaHomeWork.JavaHomeWork1;
+//Задание: Вывести все простые числа от 1 до 1000
+public class Task1 
+{
+    public static void main(String[] args) 
+    {
+        for (int i = 1; i <= 1000; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
